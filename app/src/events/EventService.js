ab488669d2f74ecb0caf1c422a0e2f3d06544e19
ryancarlson <ryan.t.carlson@cru.org>
2015-10-03 19:49:10 -0400

    Add geolocation detection to search w/in a certain range if available

--- app/src/events/EventService.js
+++ app/src/events/EventService.js
@@ -14,7 +14,7 @@
    */
   function EventService($q, $http, $filter, ApiUrl){
 
-	var getQueryDateString = function(startDate){
+	var getStartDatePathString = function(startDate){
 		/*copy startDate b/c manipulations we need to make will affect the UI if we don't*/
 		var queryDate = new Date(startDate);
 		
@@ -24,26 +24,71 @@
 		/* set the time to midnight of the day we want to retrieve data for */
 		queryDate.setHours(offsetHours,0,0,0);
 		
+		var endDate = new Date(queryDate);
+		endDate.setMinutes(queryDate.getMinutes() + 1439);
+		
 		return $filter('date')(queryDate,'yyyy-MM-ddTHH:mm:ss') + 'Z';
 	}
 	
-	this.get = function(startDate, pageSize, pageStart) {
+	var getEndDatePathString = function(startDate){
+		/*copy startDate b/c manipulations we need to make will affect the UI if we don't*/
+		var queryDate = new Date(startDate);
+		
+		/*get the offset in hours from UTC*/	
+		var offsetHours = queryDate.getTimezoneOffset() / 60;
+		
+		/* set the time to midnight of the day we want to retrieve data for */
+		queryDate.setHours(offsetHours,0,0,0);
+		
+		var endDate = new Date(queryDate);
+		endDate.setMinutes(queryDate.getMinutes() + 1439);
+		
+		return $filter('date')(endDate,'yyyy-MM-ddTHH:mm:ss') + 'Z';
+	}
+	
+	var path = function(startDate, coordinates) {
+		if(coordinates) {
+			return 'events' 
+				+ '/' + getStartDatePathString(startDate) 
+				+ '/' + getEndDatePathString(startDate)
+				+ '/' + coordinates.latitude
+				+ '/' + coordinates.longitude;
+		} else {
+			return 'events' 
+				+ '/' + getStartDatePathString(startDate) 
+				+ '/' + getEndDatePathString(startDate);
+		}
+	};
+	
+	this.get = function(startDate, pageSize, pageStart, distance) {
 		var deferredEvents = $q.defer();
 		
-
 	    var config = {
 	    	params: {
+	    		distance : distance || 30,
 				size : pageSize || 50,
 				start : pageStart || 0	
 			}
 		};
 		
-	    $http.get(ApiUrl.get() + 'startDate/' + getQueryDateString(startDate), config)
-    		.then(function(data) {
+		var lat;
+		var lon;
+		
+	 	navigator.geolocation.getCurrentPosition(function(position/*success*/){
+			$http.get(ApiUrl.get() + path(startDate,position.coords), config).then(function(data) {
     			deferredEvents.resolve(data);
 	    	}, function(reason) {
     			deferredEvents.reject(reason);
     		});
+	 	}, function(/*failure*/){
+		 	$http.get(ApiUrl.get() + path(startDate), config).then(function(data) {
+    			deferredEvents.resolve(data);
+	    	}, function(reason) {
+    			deferredEvents.reject(reason);
+    		});
+	 	});
+	 	
+	    
     	
     	return deferredEvents.promise;
 	};
